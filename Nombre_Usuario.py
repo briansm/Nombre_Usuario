dd76a933371b6dd7fa6407d78dced1640e3a33d3
Brian Morales <briansmor09@gmail.com>
2018-09-05 21:28:00 -0600

    Verifica nombre y contraseña de usuario

--- Nombre_Usuario.py
+++ Nombre_Usuario.py
@@ -8,7 +8,29 @@ def verification_user(user):
         print("Entrando...")
 
     else:
-        print("ERROR")
+        print("ERROR: No cumple con normas establecidas")
+#---------------Funcion de verificacion------------------
+def verification_password(password):
+    cont_min=0
+    cont_mayus=0
+    cont_digit=0
+    cont_simbol=0
+    if len(password)>=8:
+        for c in password:
+            if c.isdigit():
+                cont_digit=cont_digit+1
+            elif c>='a' and c<='z':
+                cont_min=cont_min+1
+            elif c>='A' and c<='Z':
+                cont_mayus=cont_mayus+1
+            else:
+                cont_simbol=cont_simbol+1
+        if cont_min>=1 and cont_digit>=1 and cont_mayus>=1 and cont_simbol>=1:
+            print("Entrando...")
+        else:
+            print("ERROR")
+    else:
+        print("ERROR: No cumple con normas establecidas")
 
 #-----------------Funcion que solicita datos-----------------
 def run():
@@ -19,6 +41,12 @@ def run():
 
                    '''))
         verification_user(user)
+        password=str(input('''
+                    --------------------------
+                    Ingrese Nombre de password:
+
+                   '''))
+        verification_password(password)
 
 
 
